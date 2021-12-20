df1d0f48a8262e7345e46b4b638ff8ea610dfc8c
Ignas75 <ignasserepka75@gmail.com>
2021-12-20 10:31:24 +0000

    Initialised project structure

---
+++ src/main/java/com/spartaglobal/musicapiproject/MusicApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.musicapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spartaglobal/musicapiproject/MusicApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.musicapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
