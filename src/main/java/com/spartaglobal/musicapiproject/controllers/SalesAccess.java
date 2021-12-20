8f95ac22a7974977bdd8b5aff3653a7e31b69066
Ria622 <75190227+Ria622@users.noreply.github.com>
2021-12-20 17:15:36 +0000

    New Records

---
+++ src/main/java/com/spartaglobal/musicapiproject/controllers/SalesAccess.java
@@ -0,0 +1,95 @@
+package com.spartaglobal.musicapiproject.controllers;
+
+import com.spartaglobal.musicapiproject.entities.*;
+import com.spartaglobal.musicapiproject.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
+public class SalesAccess {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+    private ArtistRepository artistRepository;
+    private CustomerRepository customerRepository;
+    private EmployeeRepository employeeRepository;
+    private GenreRepository genreRepository;
+    private InvoicelineRepository invoicelineRepository;
+    private InvoiceRepository invoiceRepository;
+    private MediatypeRepository mediatypeRepository;
+    private PlaylisttrackRepository playlisttrackRepository;
+    private RoleRepository roleRepository;
+    private TokenRepository tokenRepository;
+    private TrackRepository trackRepository;
+
+    @PostMapping(value="/Chinook/new-album")
+    public Album createAlbum(@Valid @RequestBody Album album1){
+        return albumRepository.save(album1);
+    }
+
+    @PostMapping(value="/Chinook/new-artist")
+    public Artist createArtist(@Valid @RequestBody Artist artist){
+        return artistRepository.save(artist);
+    }
+
+    @PostMapping(value="/Chinook/new-customer")
+    public Customer createCustomer(@Valid @RequestBody Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    @PostMapping(value="/Chinook/new-employee")
+    public Employee createEmployee(@Valid @RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    @PostMapping(value="/Chinook/new-genre")
+    public Genre createTrack(@Valid @RequestBody Genre genre1){
+        return genreRepository.save(genre1);
+    }
+
+    @PostMapping(value="/Chinook/new-invoice-line")
+    public Invoiceline createTrack(@Valid @RequestBody Invoiceline invoiceline){
+        return invoicelineRepository.save(invoiceline);
+    }
+
+    @PostMapping(value="/Chinook/new-invoice")
+    public Invoice createTrack(@Valid @RequestBody Invoice invoice){
+        return invoiceRepository.save(invoice);
+    }
+
+    @PostMapping(value="/Chinook/new-mediatype")
+    public Mediatype createTrack(@Valid @RequestBody Mediatype mediatype){
+        return mediatypeRepository.save(mediatype);
+    }
+
+    @PostMapping(value="/Chinook/new-playlist-track")
+    public Playlisttrack createTrack(@Valid @RequestBody Playlisttrack playlisttrack){
+        return playlisttrackRepository.save(playlisttrack);
+    }
+
+    @PostMapping(value="/Chinook/new-role")
+    public Role createTrack(@Valid @RequestBody Role role){
+        return roleRepository.save(role);
+    }
+
+    @PostMapping(value="/Chinook/new-token")
+    public Token createTrack(@Valid @RequestBody Token token){
+        return tokenRepository.save(token);
+    }
+
+    @PostMapping(value="/Chinook/new-track")
+    public Track createTrack(@Valid @RequestBody Track track1){
+        return trackRepository.save(track1);
+    }
+
+
+
+   /* @DeleteMapping(value="/Chinook/del-album")
+    public Album delAlbum(@PathVariable )
+    */
+
+}
--- src/main/java/com/spartaglobal/musicapiproject/repositories/AlbumRepository.java
+++ src/main/java/com/spartaglobal/musicapiproject/repositories/AlbumRepository.java
@@ -1,7 +1,8 @@
 package com.spartaglobal.musicapiproject.repositories;
 
+import com.spartaglobal.musicapiproject.entities.Album;
 import com.spartaglobal.musicapiproject.entities.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlbumRepository extends JpaRepository<Genre, Integer> {
+public interface AlbumRepository extends JpaRepository<Album, Integer> {
 }
