b42687af7a0dcfa8f48b2539b98ac6fe791e5e8e
djfrigid <electromagneticsnake@hotmail.co.uk>
2021-12-20 16:21:27 +0000

    Added a PoC auth system. Uses function and DB Table to check permissions on an endpoint by endpoint basis

--- src/main/java/com/spartaglobal/musicapiproject/controllers/AuthorizationController.java
+++ src/main/java/com/spartaglobal/musicapiproject/controllers/AuthorizationController.java
@@ -1,10 +1,12 @@
 package com.spartaglobal.musicapiproject.controllers;
 
 import com.spartaglobal.musicapiproject.entities.Employee;
+import com.spartaglobal.musicapiproject.entities.Endpointpermission;
 import com.spartaglobal.musicapiproject.entities.Role;
 import com.spartaglobal.musicapiproject.entities.Token;
 import com.spartaglobal.musicapiproject.repositories.CustomerRepository;
 import com.spartaglobal.musicapiproject.repositories.EmployeeRepository;
+import com.spartaglobal.musicapiproject.repositories.EndpointpermissionRepository;
 import com.spartaglobal.musicapiproject.repositories.TokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -32,6 +33,8 @@ public class AuthorizationController {
     private EmployeeRepository employeeRepository;
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private EndpointpermissionRepository endpointpermissionRepository;
 
     @GetMapping("/chinook/create-token")
     public ResponseEntity<String> generateNewAuthToken(@RequestParam String emailAddress){
@@ -90,6 +93,46 @@ public class AuthorizationController {
         } else {
             return new ResponseEntity<>("{\"message\": \"email address not registered\"}", headers, HttpStatus.NOT_FOUND);
         }
+    }
+
+    // THESE ARE PROOF OF CONCEPT URLS FOR THE AUTH SYSTEM
+    @GetMapping("chinook/customer-page")
+    public ResponseEntity<String> testFunction(@RequestParam String authToken){
+        if(isAuthorizedForAction(authToken, "chinook/customer-page")) {
+            return new ResponseEntity<>("Authorized", HttpStatus.OK);
+        } else return new ResponseEntity<>("Not Authorized", HttpStatus.UNAUTHORIZED);
+    }
+
+    @GetMapping("chinook/employee-page")
+    public ResponseEntity<String> testFunction2(@RequestParam String authToken){
+        if(isAuthorizedForAction(authToken, "chinook/employee-page")) {
+            return new ResponseEntity<>("Authorized", HttpStatus.OK);
+        } else return new ResponseEntity<>("Not Authorized", HttpStatus.UNAUTHORIZED);
+    }
+
+    @GetMapping("chinook/admin-page")
+    public ResponseEntity<String> testFunction3(@RequestParam String authToken){
+        if(isAuthorizedForAction(authToken, "chinook/admin-page")) {
+            return new ResponseEntity<>("Authorized", HttpStatus.OK);
+        } else return new ResponseEntity<>("Not Authorized", HttpStatus.UNAUTHORIZED);
+    }
+
+    // Auth function, called with the token in a request and the endpoint hardcoded from inside the function tied to that endpoint
+    public boolean isAuthorizedForAction(String authToken, String endpoint){
+        if(tokenRepository.existsByAuthToken(authToken)){
+            Token token = tokenRepository.getByAuthToken(authToken);
+            Endpointpermission permissions = endpointpermissionRepository.getByUrl(endpoint);
+            switch (token.getRoleID().getId()){
+                case 1 -> {
+                    if (permissions.getIsForAdmins()) return true;
+                } case 2 -> {
+                    if(permissions.getIsForStaff()) return true;
+                } case 3 -> {
+                    if (permissions.getIsForCustomer()) return true;
+                }default -> {return false;}
+            }
+        }
+        return false;
     }
 
 }
