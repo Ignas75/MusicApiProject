35931637a591a1c2fb5ed2513c1605d569108df6
leo-hoang <leohoang92@gmail.com>
2021-12-20 16:07:22 +0000

    Added UserSearchable Interface and used classes implement it

--- src/main/java/com/spartaglobal/musicapiproject/controllers/UserContentSearch.java
+++ src/main/java/com/spartaglobal/musicapiproject/controllers/UserContentSearch.java
@@ -43,7 +43,7 @@ public class UserContentSearch {
     @GetMapping (value = "/chinook/search")
     public List<? extends UserSearchable> getQuery()
     {
-        trackRepository.findOne()
+
         return null;
     }
 
