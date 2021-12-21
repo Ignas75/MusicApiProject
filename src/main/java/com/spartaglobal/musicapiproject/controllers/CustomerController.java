803d355384986c2f75048b2de217f2fc5feac2ba
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2021-12-21 16:46:11 +0000

    Customer Controlller

--- src/main/java/com/spartaglobal/musicapiproject/controllers/CustomerController.java
+++ src/main/java/com/spartaglobal/musicapiproject/controllers/CustomerController.java
@@ -6,10 +6,23 @@ import com.spartaglobal.musicapiproject.repositories.CustomerRepository;
 import com.spartaglobal.musicapiproject.repositories.TokenRepository;
 import com.spartaglobal.musicapiproject.services.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+
+/***
+ * Please add following lines of sql query into the database after the end
+ */
+
+
+
+
+
 
 @RestController
 public class CustomerController {
@@ -42,6 +55,66 @@ public class CustomerController {
             }
         }
         return new ResponseEntity<String>("{\"message\":\"You're not authorized!\"}", headers, HttpStatus.OK);
+    }
+
+
+    //get the customer detail based on the auth token
+    @GetMapping(value = "chinook/customer")
+    public ResponseEntity<?> requestCustomerDetails(@RequestHeader("Authorization") String authToken){
+        String token[] = authToken.split(" ");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-type", "application/json");
+        if (aS.isAuthorizedForAction(token[1], "chinook/customer")){
+            Token userToken = tokenRepository.getByAuthToken(token[1]);
+            Customer customer = customerRepository.getCustomerByEmail(userToken.getEmail());
+            if (customer != null)
+                return ResponseEntity.ok(customer);
+        }
+        return new ResponseEntity<>("{\"message\":\"Invalid Request\"}",headers, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @DeleteMapping(value = "chinook/sales/delete/customer")
+    public ResponseEntity<?> deleteCustomerByCustomerID(@RequestParam Integer customerID, @RequestHeader("Authorization") String authToken){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-type", "application/json");
+        String token[] = authToken.split("chinook/sales/delete/customer");
+        if (aS.isAuthorizedForAction(token[1], ""))
+        return new ResponseEntity<>("{\"message\":\"Invalid Request\"}",headers, HttpStatus.BAD_REQUEST);
+    }
+
+
+
+
+
+
+    @PatchMapping(value="chinook/sales/update")
+    public ResponseEntity<?> updateCustomerByEmailAddress( @RequestParam Customer customer1, @RequestHeader("Authorization") String authToken){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-type","application/json");
+        String token[] = authToken.split("");
+        if(aS.isAuthorizedForAction(token[1],"chinook/sales/update")){
+            Token userToken = tokenRepository.getByAuthToken(token[1]);
+            Customer customer = customerRepository.getCustomerByEmail(userToken.getEmail());
+            if(customer != null)
+                return ResponseEntity.ok(customer);
+        }
+        Optional<Customer> customer = customerRepository.findById(customer1.getId());
+        if(customer.isEmpty()) {
+            return new ResponseEntity<String>("{\"message\":\"Customer doesn't exist" + customer1.getId() + "\"}", headers, HttpStatus.OK);
+        }
+        customer.get().setFirstName(customer1.getFirstName());
+        customer.get().setLastName(customer1.getLastName());
+        customer.get().setCompany(customer1.getCompany());
+        customer.get().setAddress(customer1.getAddress());
+        customer.get().setCity(customer1.getCity());
+        customer.get().setState(customer1.getState());
+        customer.get().setCountry(customer1.getCountry());
+        customer.get().setPostalCode(customer1.getPostalCode());
+        customer.get().setPhone(customer1.getPhone());
+        customer.get().setFax(customer1.getFax());
+        customer.get().setEmail(customer1.getEmail());
+        String message = "{\"messsage\" : \"Customer Update\", \"film\":";
     }
 }
 
