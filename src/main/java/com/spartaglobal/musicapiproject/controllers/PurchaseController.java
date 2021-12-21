51de7fb0c5140d2537e52a549d64a304be1a616c
Ignas75 <ignasserepka75@gmail.com>
2021-12-21 11:47:27 +0000

    Mostly implemented purchaseAlbum

--- src/main/java/com/spartaglobal/musicapiproject/controllers/PurchaseController.java
+++ src/main/java/com/spartaglobal/musicapiproject/controllers/PurchaseController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.swing.text.html.Option;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class PurchaseController {
     @Autowired
@@ -56,12 +59,31 @@ public class PurchaseController {
         return userTracks;
     }
 
+    public List<Track> getUserPurchasedTracksFromAlbum(Integer customerId, Integer albumId){
+        List<Track> purchasedTracksFromAlbum = new ArrayList<>();
+        Optional<Album> findAlbum = albumRepository.findById(albumId);
+        Optional<Customer> findCustomer = customerRepository.findById(customerId);
+        if(findAlbum.isEmpty() || findCustomer.isEmpty()){
+            return purchasedTracksFromAlbum;
+        }
+        List<Track> userTracks = getUserTracks(customerId);
+        for(Track track: userTracks){
+            if(track.getAlbumId().getId().equals(albumId)){
+                purchasedTracksFromAlbum.add(track);
+            }
+        }
+        return purchasedTracksFromAlbum;
+    }
+
+    public List<Track> getAlbumTracks(Integer albumId){
+        return trackRepository.findAll().stream().filter(s -> s.getAlbumId().getId().equals(albumId)).toList();
+    }
+
 
     // return album price
-    // needs to check for discounts in a discount table and whether they still apply
+    // TODO: needs to check for discounts in a discount table and whether they still apply
     @GetMapping(value = "album/cost")
-    public ResponseEntity<String> getAlbumCost(@RequestParam Integer albumId, @RequestParam Integer userId){
-        Integer customerId = customerRepository.getById(userId).getId();
+    public ResponseEntity<String> getAlbumCost(@RequestParam Integer albumId, @RequestParam Integer customerId){
         List<Track> userTracks = getUserTracks(customerId);
 
         BigDecimal totalCost = new BigDecimal(0);
@@ -76,10 +98,53 @@ public class PurchaseController {
     }
 
 
-
-
     // purchase an album
     // need already purchased tracks from the user that belong to the current album
     // what if the same song is on another album?
+    @PostMapping(value = "album/purchase")
+    public ResponseEntity<String> purchaseAlbum(@RequestParam Integer albumId, @RequestParam Integer customerId,
+                                                @RequestParam String billingAddress, @RequestParam String billingCity,
+                                                @RequestParam String billingCountry, @RequestParam String postalCode){
+        Optional<Customer> findCustomer = customerRepository.findById(customerId);
+        // checking if the Ids are valid
+        if(findCustomer.isEmpty()){
+            return new ResponseEntity<>("No customer entry exists with that id", HttpStatus.NOT_FOUND);
+        }
+        Optional<Album> findAlbum = albumRepository.findById(albumId);
+        if(findAlbum.isEmpty()){
+            return new ResponseEntity<>("No album entry exists with that id", HttpStatus.NOT_FOUND);
+        }
+        Customer customer = findCustomer.get();
+
+        List<Track> purchasedTracksFromAlbum = getUserPurchasedTracksFromAlbum(customerId, albumId);
+        List<Track> albumTracks = getAlbumTracks(albumId);
+        List<Track> tracksToPurchase = new ArrayList<>();
+        BigDecimal totalCost = new BigDecimal(0);
+        for(Track track: albumTracks){
+            if(!purchasedTracksFromAlbum.contains(track)){
+                tracksToPurchase.add(track);
+                totalCost.add(track.getUnitPrice());
+            }
+        }
+        if(!tracksToPurchase.isEmpty()){
+            Invoice invoice = new Invoice();
+            invoice.setBillingAddress(billingAddress);
+            invoice.setBillingCity(billingCity);
+            invoice.setBillingCountry(billingCountry);
+            invoice.setBillingPostalCode(postalCode);
+            invoice.setCustomerId(customer);
+            invoice.setTotal(totalCost);
+            invoiceRepository.save(invoice);
+            for(Track track: tracksToPurchase){
+                Invoiceline invoiceline = new Invoiceline();
+                invoiceline.setInvoiceId(invoice);
+                invoiceline.setQuantity(1);
+                invoiceline.setUnitPrice(track.getUnitPrice());
+                invoicelineRepository.save(invoiceline);
+            }
+        }
+        return new ResponseEntity<>("Successfully registered purchase of " + tracksToPurchase.size() + " tracks",
+                HttpStatus.OK);
+    }
 
 }
